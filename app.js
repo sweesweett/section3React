7ef8ab9e06bd1215beaa22e837ffbc94777617c9
sweesweett <sweesweet@naver.com>
2022-08-17 18:16:18 +0900

    섹션4 프록시+ crud서버구현

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const { dummydata, dummyLike } = require('./dummy');
+let newDummyLike = [...dummyLike];
+let newDummyData = [...dummydata];
 const dotenv = require('dotenv');
 dotenv.config();
 var express = require('express');
@@ -7,11 +10,11 @@ var app = express();
 app.use(
   cors({
     origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'OPTIONS'],
+    methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT'],
     credentials: true,
   })
 );
-
+app.use(express.json());
 app.get('/search/blog', function (req, res) {
   var api_url =
     'https://openapi.naver.com/v1/search/blog?query=' +
@@ -28,29 +31,45 @@ app.get('/search/blog', function (req, res) {
       'X-Naver-Client-Secret': process.env.CLIENT_SECRET,
     },
   };
-  axios.get(api_url, options).then(({ data }) => res.status(200).json(data));
-  // axios
-  //   .get({
-  //     params: {
-  //       query: uri,
-  //       display: 20,
-  //       start: 1,
-  //       sort: 'sim',
-  //     },
-  //     headers: {
-  //       'X-Naver-Client-Id': process.env.REACT_APP_CLIENT_ID,
-  //       'X-Naver-Client-Secret': process.env.REACT_APP_CLIENT_SECRET,
-  //     },
-  //   })
-
-  //   .then(({ data }) => console.log(data))
-  //   .catch((err) => console.log(err));
-  // //process.env.REACT_APP_CLIENT_ID
-  // //process.env.REACT_APP_CLIENT_SECRET
-  // console.log(result);
+  axios
+    .get(api_url, options)
+    .then(({ data }) => res.status(200).json(data))
+    .catch((err) => res.status(400).send(err.message));
 });
-app.listen(4000, function () {
-  console.log(
-    'http://127.0.0.1:4000/search/blog?query=검색어 app listening on port 4000!'
+app.get('/like', function (req, res) {
+  return res.json(dummyLike);
+});
+app.post('/like', function (req, res) {
+  if (dummyLike.findIndex((el) => el.title === req.body.title) === -1) {
+    dummyLike.push(req.body);
+  }
+});
+app.delete('/like/:id', function (req, res) {
+  newDummyLike = newDummyLike.filter((el) => el.id !== req.params.id);
+  return res.json(newDummyLike);
+});
+//------------------------
+app.get('/todo', function (req, res) {
+  return res.json(newDummyData);
+});
+app.post('/todo', function (req, res) {
+  newDummyData = [req.body, ...newDummyData];
+  return res.json(newDummyData);
+});
+app.put('/todo', function (req, res) {
+  console.log(req.body);
+  newDummyData = newDummyData.map((el) =>
+    el.id === req.body.id ? { el, ...req.body } : el
   );
+  return res.json(newDummyData);
+});
+app.delete('/todo/:id', function (req, res) {
+  console.log(req.params.id);
+  newDummyData = newDummyData.filter((el) => el.id != req.params.id);
+  console.log(newDummyData);
+  return res.json(newDummyData);
+});
+
+app.listen(4000, function () {
+  console.log('app listening on port 4000!');
 });
---
+++ dummy.js
@@ -0,0 +1,70 @@
+module.exports.dummydata = [
+  {
+    id: '2ea',
+    title: '테스트중',
+    content: '지금 테스트중이지렁',
+  },
+  {
+    id: '1dwt',
+    title: '테스트중2',
+    checked: 'css구조 다시해야해눈물난당',
+  },
+];
+
+module.exports.dummyLike = [
+  {
+    id: 'cfc1ae0-66dc-eb17-572a-4d2712d4f683',
+    title:
+      '<b>합정</b>역 <b>대존맛</b> 보스 고기집, 마부자생삼겹살 김치찌개 <b>합정</b>',
+    link: 'https://blog.naver.com/jalsarable?Redirect=Log&logNo=222622380265',
+    description:
+      '친구가 <b>합정</b>에 있는 생삼겹살 맛집을 찾아와서 뿌수러 갔다,, 블로그 쓰는 동안에도 침고인다 ^.ㅠ 마부자생삼겹살 <b>합정</b>점 <b>합정</b>역 8번출구 부근에 위치한 마부자생삼겹살 <b>합정</b>점! 휴.,, 침고인다, <b>대존맛</b>탱 ㅠ... ',
+    bloggername: '라블이의 잘 사는 라이프, 잘살라블 !',
+    bloggerlink: 'https://blog.naver.com/jalsarable',
+    postdate: '20220115',
+  },
+  {
+    id: 'ea0abd4-d1cc-d26d-c6d3-6dfcdfa1db2a',
+    title:
+      '<b>합정</b> 중국집 은하루 - 망원 <b>합정</b> <b>로컬</b>들이 <b>추천</b>하는 <b>맛집</b>',
+    link: 'https://blog.naver.com/dbsdl4500?Redirect=Log&logNo=222811122566',
+    description:
+      '그래서 좀 남겼지만 맛있었던 메뉴! <b>합정</b> 은하루 <b>합정</b> <b>맛집</b>으로 대<b>추천</b> 특히 탕수육 최고 <b>합정</b> 은하루 [영업시간] 11:00 - 22:00 (일요일 휴무) 3시~5시 브레이크타임 주차 3대 가능, 배달, 단체석, 예약가능',
+    bloggername: '맛케터의 먹고보고서',
+    bloggerlink: 'https://blog.naver.com/dbsdl4500',
+    postdate: '20220714',
+  },
+  {
+    id: 'bbee47c-7dc3-23cc-bb58-25c4d2718e',
+    title:
+      '<b>합정</b>/상수 <b>로컬</b><b>맛집</b> :: 닭볶음탕이 맛있는 <b>합정</b>과 상수... ',
+    link: 'https://blog.naver.com/suyeon01303?Redirect=Log&logNo=222794588246',
+    description:
+      '그래서 오랜만에 간술 (간 죽이는 술 ㅎ) 매콤한 국물 쫙 스며든 당면 <b>존맛</b>탱 맛있게 익어가는... 걸어야하지만 <b>로컬</b><b>맛집</b>은 그정도 고생쯤은..^^ 홍대,<b>합정</b>,상수 쪽 맛있는 닭볶음탕, 육전 먹고싶으면 <b>추천</b>하는... ',
+    bloggername: 'SUYEON LOG',
+    bloggerlink: 'https://blog.naver.com/suyeon01303',
+    postdate: '20220630',
+  },
+  {
+    id: '603a840-062f-be87-57b5-eb318835a0',
+    title:
+      '<b>합정</b> 한우 <b>맛집</b> 바람길 유명하길래 <b>로컬</b>이 가봤지',
+    link: 'https://blog.naver.com/glqgkqehdwl?Redirect=Log&logNo=222569904107',
+    description:
+      '여기 물냉면의 육수는 시중에서 쉽게 접하는 자극적인 맛도 아니고 사장님의 <b>추천</b> 조리법으로... 역시 고기는 역시 옳다.ㅋㅋㅋ #<b>합정</b>한우<b>맛집</b> #바람길 #<b>합정</b>바람길 #한우 #소고기 #<b>로컬</b><b>맛집</b> #홀릭이랑혜윰이랑... ',
+    bloggername: 'a.k.a holic',
+    bloggerlink: 'https://blog.naver.com/glqgkqehdwl',
+    postdate: '20211116',
+  },
+  {
+    id: '5bba328-b33d-aefd-dd35-7f440d546012',
+    title:
+      '★내돈내산★ <b>합정</b> <b>맛집</b> &quot;함반&quot; 대<b>존맛</b>. 테이블링 필수!!... ',
+    link: 'https://blog.naver.com/yjh02250?Redirect=Log&logNo=222840139431',
+    description:
+      '또륵 #함반 #<b>합정</b><b>맛집</b> 숯불함박 다란 들어왔다~~~~~ 하이볼세트 하이볼 맛있겠다.. 하지만 반주를 즐기는... 솔직히 3입컷이면 끝나는데 아껴먹었다 흐흨 닭고기 함반이랑 어울리는 <b>추천</b> 소스로는 바질소스... ',
+    bloggername: '맛집게이터',
+    bloggerlink: 'https://blog.naver.com/yjh02250',
+    postdate: '20220805',
+  },
+];
